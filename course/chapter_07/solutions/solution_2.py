51eb2919f68356859c95db11bc987892b8282db2
dixfellation <zamsocyrenzapov@gmail.com>
2025-02-19 17:08:43 +0800

    .

--- course/chapter_07/solutions/solution_2.py
+++ course/chapter_07/solutions/solution_2.py
@@ -1,6 +1,7 @@
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-for i in numbers // 2 == 0:
-        print(numbers // 2)
 for i in numbers:
-    if numbers // 1 ==0:
-        print(numbers // 1)
+    if numbers % 2 == 0:
+        print(i)
+for i in numbers:
+    if numbers % 2 ==1:
+        print(i)
