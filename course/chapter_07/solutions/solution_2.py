f3065f8df55d8ced90fecfe775d693b4a119c494
dixfellation <zamsocyrenzapov@gmail.com>
2025-02-19 16:59:12 +0800

    .

--- course/chapter_07/solutions/solution_2.py
+++ course/chapter_07/solutions/solution_2.py
@@ -1,10 +1,6 @@
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-e_numbers = []
-o_numbers = []
-for n in numbers:
-    if n % 2 == 0:
-        e_numbers.append(numbers)
-    else:
-        o_numbers.append(numbers)
-print(", ".join(map(str, e_numbers)))
-print(", ".join(map(str, o_numbers)))
+for i in numbers // 2 == 0:
+        print(numbers // 2)
+for i in numbers:
+    if numbers // 1 ==0:
+        print(numbers // 1)
---
+++ course/chapter_07/solutions/solution_3.py
@@ -0,0 +1,3 @@
+numbers = [3, 17, 6, 12, 9, 21, 5]
+max_element=(21)
+print("Максимальный элемент:" max_element)
